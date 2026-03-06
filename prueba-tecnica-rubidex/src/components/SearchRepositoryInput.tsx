6c156c8e69951c74f4be49208c41b7c01650b59c
Ricardo Agustin Moran Rivas <ricardo.moran@elektra.com.mx>
2026-03-05 22:16:00 -0600

    add: añade componente SearchRepositoryInput

---
+++ prueba-tecnica-rubidex/src/components/SearchRepositoryInput.tsx
@@ -0,0 +1,52 @@
+import type { InputHTMLAttributes } from "react";
+import { cn } from "../utils/cn";
+import Input from "./ui/Input";
+import { Search } from "lucide-react";
+
+interface SearchRepositoryInputProps extends Omit<
+  InputHTMLAttributes<HTMLInputElement>,
+  "onChange"
+> {
+  onChange: (value: string) => void;
+}
+
+export function SearchRepositoryInput({
+  value,
+  onChange,
+  name,
+  placeholder,
+  id,
+  className,
+  ...props
+}: SearchRepositoryInputProps) {
+  const handleOnChange = (
+    e: React.ChangeEvent<HTMLInputElement, HTMLInputElement>,
+  ) => {
+    if (typeof onChange === "function") {
+      onChange(e.target.value);
+    }
+  };
+
+  const _id = id ?? name;
+
+  return (
+    <div className="flex items-center border border-text/35 rounded-md pl-2  focus-within:border-accent">
+      <label htmlFor={_id}>
+        <Search className="text-text/65" />
+      </label>
+      <Input
+        id={_id}
+        name={name}
+        type="text"
+        value={value}
+        onChange={handleOnChange}
+        placeholder={placeholder}
+        className={cn(
+          "px-2 rounded-md h-10 focus-visible:outline-none",
+          className,
+        )}
+        {...props}
+      />
+    </div>
+  );
+}

5dcab0e78f38fa2240e0fe43eeee6edd1ab84371
Ricardo Agustin Moran Rivas <ricardo.moran@elektra.com.mx>
2026-03-05 22:18:31 -0600

    add: añade servicio para consultar repositorios por username de github

---
+++ prueba-tecnica-rubidex/src/services/getRespositoriesList.ts
@@ -0,0 +1,30 @@
+import type { RepositoryCardProps } from "../components/RepositoryCard";
+
+export type getRepositoriesListInput = {
+  username: string;
+};
+
+export interface getRepositoriesResponse extends RepositoryCardProps {
+  id: number;
+}
+
+export async function getRepositoriesList({
+  username,
+}: getRepositoriesListInput): Promise<getRepositoriesResponse[] | undefined> {
+  try {
+    const response = await fetch(
+      `https://api.github.com/users/${username}/repos`,
+    );
+
+    const result = await response.json();
+
+    if (!response.ok) {
+      throw new Error(result.message);
+    }
+
+    return result;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
